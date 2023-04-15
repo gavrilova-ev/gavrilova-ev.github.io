d6a38e9c7bc432fb43aa2655f38531e071bdeeb0
Ekaterina Kruse <gavrilova.ev@gmail.com>
2023-04-15 15:37:38 +0200

    Update Webcomponent.js

--- CustomWidget/Webcomponent.js
+++ CustomWidget/Webcomponent.js
@@ -1,8 +1,8 @@
-(function () { 
+(function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
     `<button type="button" id="myBtn">Helper Button</button>` ;   
-   
+
     class PerformanceHelp extends HTMLElement {
         constructor() {
             super();
@@ -10,7 +10,7 @@
         }
 
         init() {            
-              
+
             let shadowRoot = this.attachShadow({mode: "open"});
             shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this.addEventListener("click", event => {
@@ -19,11 +19,13 @@
             this.dispatchEvent(event);
             });           
         }
-        
+
         fireChanged() {
             console.log("OnClick Triggered");     
-            
+
         }        
-       
+
+    }
+
     customElements.define('custom-button', PerformanceHelp);
 })();
